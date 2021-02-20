96cf43ffe69d36b245eb444e74beddaecd0e8c30
SemiMyth <SemiMyth@gmail.com>
2021-02-19 19:51:07 -0500

    A bunch of stuff

---
+++ TowerDefenceMiniProject/Assets/Scripts/MouseController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class MouseController : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("working");
+        if (!EventSystem.current.IsPointerOverGameObject())
+        {
+            ShowPosition();
+        }
+
+        void ShowPosition()
+        {
+            Ray inputRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(inputRay, out hit))
+            {
+                Debug.Log(hit.point);
+                if (hit.point.z >= 0 && hit.point.z <= Measurements.mapZ
+                    && hit.point.x >= 0 && hit.point.x <= Measurements.mapX)
+                {
+                    gameObject.SetActive(true);
+                    transform.localPosition = hit.point;
+                }
+                else { gameObject.SetActive(false); }
+                //Debug.Log(hit.point);
+                //HexCell currentCell = grid.GetCell(hit.point); 
+            }
+        }
+    }
+}
