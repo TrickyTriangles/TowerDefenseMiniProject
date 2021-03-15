eed2539d91f720ff1fb44a942088f77724e75043
SemiMyth <SemiMyth@gmail.com>
2021-03-14 20:03:26 -0400

    UpdatesToGridMaster

---
+++ TowerDefenceMiniProject/Assets/Scripts/MapController.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapController : MonoBehaviour
+{
+    public Transform tile;
+    public Transform master;
+    public List <TileData> playGrid;
+
+    TileData[] tiles;
+
+    private void Start()
+    {
+        
+        playGrid = new List<TileData>();
+        InstantiateMap();
+    }
+
+
+    //***************************************//
+
+    void InstantiateMap()
+    {
+        int makeX = Measurements.mapX;
+        int makeZ = Measurements.mapZ;
+        tiles = new TileData[makeX * makeZ];
+        int i = 0;
+
+        for (int ix=0; ix< makeX; ix++)
+        {
+            for(int iz = 0; iz< makeZ; iz++)
+            {
+                CreateTile(ix, iz, i++);
+
+            }
+        }
+    }
+
+    void CreateTile (int x, int z, int i)
+    {
+        TileData newTile = tiles[i] = Instantiate(tile).GetComponent<TileData>();
+        newTile.transform.localPosition = new Vector3(x, 0, z);
+        newTile.transform.parent = master;
+
+        playGrid.Add(newTile);
+
+
+        if (x > 0)
+        {
+            TileData lastTile = playGrid[i - Measurements.mapZ];
+            newTile.SetNeighbor(TileDirection.W, lastTile);
+        }
+
+        if (z > 0)
+        {
+            TileData oppositeTile = playGrid[i - 1];
+            newTile.SetNeighbor(TileDirection.S, oppositeTile);
+        }
+    }
+}
---
+++ TowerDefenceMiniProject/Assets/Scripts/TileDirection.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum TileDirection
+{
+    N, E, S, W
+}
+
+public static class TileDirectionExtensions
+{
+    public static TileDirection Opposite(this TileDirection direction)
+    {
+        //IF less than 3, direction +3, ELSE, direction -3
+        if ((int)direction < 2) { return direction + 2; } // ?
+        else { return direction - 2; }  // :
+    }
+
+    public static TileDirection Previous(this TileDirection direction)
+    { // if NOT start of array, -1, if start of array, go to end of array
+        return direction == TileDirection.N ? TileDirection.W : (direction - 1);
+    }
+
+    public static TileDirection Next(this TileDirection direction)
+    {
+        return direction == TileDirection.W ? TileDirection.N : (direction + 1);
+    }
+}
